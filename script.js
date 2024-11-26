0c0231c6e1203612477b0ebc956ee4039413c75c
TheFeast-lang <thefeast971@gmail.com>
2024-11-26 14:05:42 +0800

    updated

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
 class LeafletMap {
-    constructor(containerId, center, zoom) {
-      this.map = L.map(containerId, {
-        center: center,
-        zoom: zoom,
-        dragging: true, 
-        zoomControl: false,
-        scrollWheelZoom: false,
-        doubleClickZoom: false,
-        boxZoom: false, 
-        touchZoom: false, 
-        tap: false 
-      });
+
+  constructor(containerId, center, zoom) {
+      this.map = L.map(containerId).setView(center, zoom);
       this.initTileLayer();
-    }
-  
-    initTileLayer() {
+  }
+
+  initTileLayer() {
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          maxZoom: 19,
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(this.map);
-    }
-  
-    addMarker(lat, lng, message, imageUrl, id) {
+  }
+
+  addMarker(lat, lng, message) {
       const marker = L.marker([lat, lng]).addTo(this.map);
       marker.bindPopup(message);
-  
-      marker.on('click', () => {
-        marker.bindPopup(`<img src="${imageUrl}" alt="${message}" style="width: 100px; height: auto;" />`);
-        marker.openPopup();
-      });
-    }
-  
-    loadMarkersFromJson(url) {
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          data.garbageData.forEach(item => {
-            this.addMarker(
-              item.location.lat, 
-              item.location.lng, 
-              item.name, 
-              item.image, 
-              item.id
-            );
-          });
-        })
-        .catch(error => console.error('Error loading markers:', error));
-    }
   }
-  
-  const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
-  myMap.loadMarkersFromJson('app.json');
-  
+
+  loadMarkersFromJson(url) {
+      fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              data.forEach(marker => {
+                  this.addMarker(marker.latitude, marker.longitude, marker.message);
+              });
+          })
+          .catch(error => console.error('Error loading markers:', error));
+  }
+}
+
+const myMap = new LeafletMap('map', [8.3600043, 124.8655281], 17);
+
+/*
+myMap.addMarker (8.4003566, 124.8780386, 'My home');
+*/
+
+myMap.addMarker(8.359836, 124.869316, 'Garden 1');
+myMap.addMarker(8.359640, 124.869255, 'Garden 2');
+myMap.addMarker(8.360063, 124.869374, 'Garden 3');
+myMap.addMarker( 8.360272, 124.869434, 'Garden 4');
